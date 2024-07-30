9144d14a2d580e5e1c492e120961c9245fb27a12
Alexander <107767827+entgorbunov@users.noreply.github.com>
2024-07-30 22:57:50 +0500

    git init

---
+++ src/main/java/sorokin/java/course/bookingservicepavelsorokin/BookingServicePavelSorokinApplication.java
@@ -0,0 +1,13 @@
+package sorokin.java.course.bookingservicepavelsorokin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServicePavelSorokinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingServicePavelSorokinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sorokin/java/course/bookingservicepavelsorokin/BookingServicePavelSorokinApplicationTests.java
@@ -0,0 +1,13 @@
+package sorokin.java.course.bookingservicepavelsorokin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServicePavelSorokinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
